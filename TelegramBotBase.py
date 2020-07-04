c031d6afdb04815c88a3e85e390076fb6e1bc5a5
Paul Wenzlaff <Paulwenzlaff@gmail.com>
2020-07-04 14:01:09 +0200

    Update TelegramBotBase.py

--- TelegramBotBase.py
+++ TelegramBotBase.py
@@ -7,8 +7,8 @@ token = lines[0].strip()
 bot = telebot.TeleBot(str(token))
 
 @bot.message_handler(commands=['Hello'])
-def StartCamera(message):
-    SetCameraValue(True)
+def SayHello(message):
+    bot.reply_to(message, "Hello")
 
 @bot.message_handler(func=lambda message: True)
 def EchoAll(message):
